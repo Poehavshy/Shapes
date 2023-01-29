eb17bcfef4d282ae64285f358b15965350e21117
Demirchyan Denis <denis.demirchyan@gmail.com>
2023-01-29 19:56:33 +0300

    Добалено консольное приложение

---
+++ Application/Program.cs
@@ -0,0 +1,24 @@
+﻿using Shapes.Interfaces;
+using Shapes.Models;
+
+IShape shape;
+
+Console.WriteLine("Выберите фигуру:");
+Console.WriteLine("1. Круг");
+Console.WriteLine("2. Треугольник");
+var choose = Console.ReadLine() ?? "1";
+
+switch (choose[0])
+{
+    case '1':
+        shape = new Circle(5);
+        break;
+    case '2':
+        shape = new Triangle(3, 4, 5);
+        break;
+    default:
+        Console.WriteLine("Некорректный выбор.");
+        return;
+}
+
+Console.WriteLine($"Площадь равна: {shape.CalculateArea()}");
